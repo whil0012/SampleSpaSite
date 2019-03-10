3368b45c59b1c08e25007e3410b615964807349d
whil0012 <whil0012@users.noreply.github.com>
2019-03-10 18:22:22 -0400

    Update jslint comments / settings

--- ch_05/js/spa.util_b.js
+++ ch_05/js/spa.util_b.js
@@ -8,7 +8,8 @@
  * MIT License
 */
 
-/*jslint         browser : true, continue : true,
+/*
+jslint         browser : true, continue : true,
   devel  : true, indent  : 2,    maxerr   : 50,
   newcap : true, nomen   : true, plusplus : true,
   regexp : true, sloppy  : true, vars     : false,
